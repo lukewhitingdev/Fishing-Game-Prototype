62601b7bd6a85edd4e19db8ee3e674dd3551cc9c
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2024-02-12 13:01:01 +0000

    Initial Commit

---
+++ Source/FishingGame/FishingGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FishingGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISHINGGAME_API AFishingGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
