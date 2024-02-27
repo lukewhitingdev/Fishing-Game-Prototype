fbc2b763ab4921d55753fa28e0f4f16b99fecd81
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2024-02-27 22:14:41 +0000

    Added a interact trigger and ability to interact to the player character / controller

--- Source/FishingGame/Private/FG_Character.h
+++ Source/FishingGame/Private/FG_Character.h
@@ -5,23 +5,31 @@
 #include "CoreMinimal.h"
 #include "IFG_MoveableCharacter.h"
 #include "GameFramework/Character.h"
+#include "Interfaces/FG_IUseInteracts.h"
 #include "FG_Character.generated.h"
 
+class UFG_CharacterInteractComponent;
+class UBoxComponent;
+
 UCLASS()
-class AFG_Character : public ACharacter, public IFG_MoveableCharacter
+class AFG_Character : public ACharacter, public IFG_MoveableCharacter, public IFG_IUseInteracts
 {
 	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFG_Character();
 
 public:
 	UPROPERTY(EditAnywhere, Category="Movement")
 	int CharacterMovementSpeed = 10;
 
-	// Sets default values for this character's properties
-	AFG_Character();
+private:
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* InteractTrigger;
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	UPROPERTY()
+	UFG_CharacterInteractComponent* InteractComponent;
 
 private:
 	// IFG_MoveableCharacter Interface
@@ -31,7 +39,24 @@ private:
 	virtual void MoveRight(float Value) override;
 	// END IFG_MoveableCharacter Interface.
 
-public:	
+	// IFG_UseInteracts Interface
+	virtual void Interact() override;
+	// END IFG_UseInteracts Interface.
+
+	// Sets the controller rotation, will assert if no controller is possesing this character.
+	void SetControllerRotation(const FRotator& Rotation);
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnInteractOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnInteractOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
