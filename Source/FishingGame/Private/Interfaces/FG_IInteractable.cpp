ac27e29b391831d0e028e7d95e71eb835dd22e85
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2024-02-27 22:14:02 +0000

    Added the interactable interface
    This interface is for anything that is interactable by other elements of the world

---
+++ Source/FishingGame/Private/Interfaces/FG_IInteractable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/FG_IInteractable.h"
+
+// Add default functionality here for any IFG_IInteractable functions that are not pure virtual.
