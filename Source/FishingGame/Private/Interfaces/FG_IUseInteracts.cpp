f2c392bca9c18d01ba27d77ce714e614cec93e2a
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2024-02-27 22:13:40 +0000

    Added use interacts interface
    This will define anything that is able to interact with the world through the Interact() func

---
+++ Source/FishingGame/Private/Interfaces/FG_IUseInteracts.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/FG_IUseInteracts.h"
+
+// Add default functionality here for any IFG_IUseInteracts functions that are not pure virtual.
